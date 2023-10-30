50b1ccc6032be3c070212639dbef826f2a9d0d46
Logan Winnie <loganwinnier@gmail.com>
2023-10-30 19:12:12 -0400

    pokedex done

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,3 +1,18 @@
-function Pokecard ({id, name, type, base_experience}){
+import "./Pokecard.css";
+const BASE_URL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`;
 
+function Pokecard({ pokemon }) {
+    const { id, name, type, base_experience } = pokemon;
+
+    const image = `${ BASE_URL }${ id }.png`;
+    return (
+        <div className="Pokecard">
+            <h2>{name}</h2>
+            <img src={image} alt={name + "image"}></img>
+            <h3>{type}</h3>
+            <p>{base_experience}</p>
+        </div>
+    );
 }
+
+export default Pokecard;
